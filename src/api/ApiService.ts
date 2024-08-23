c41e56a7835d0baa091504e1d93db031bc5ce299
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2024-08-23 15:10:24 -0300

    settings apiservices

--- src/api/ApiService.ts
+++ src/api/ApiService.ts
@@ -2,7 +2,7 @@
 import React from 'react'
 import axios from 'axios'
 
-const url = 'https://api-fractus.herokuapp.com/api/'
+const url = 'https://api-fractus-v4-653b078087a9.herokuapp.com/api/'
 
 export const ApiService = axios.create({
   baseURL: url,
