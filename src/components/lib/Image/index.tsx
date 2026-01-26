14c8016bc279b400cae075a1fd6023999a592b00
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2026-01-26 15:55:22 -0300

    Update index.tsx

--- src/components/lib/Image/index.tsx
+++ src/components/lib/Image/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Image(props: TypeImage) {
-  const urlBase = "http://app-fractus.orkneytech.com.br/assets/images/"
+  const urlBase = "https://storage.googleapis.com/fractus-bucket/"
   const pathImage = `${urlBase}${props.src}`
   const text = `${props.alt}`
   const width = `${props.width}`
